576f3a7baaa391d33eba4f24704662bc586df21a
genie <banggijin>
2022-01-19 17:21:08 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,70 +1,8 @@
-# Getting Started with Create React App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Get information of a single asset
+curl --request GET \
+     --url https://api.opensea.io/api/v1/asset/0xa342f5d851e866e18ff98f351f2c6637f4478db5/53343204100803765692379285688171671302437967278842259121980540727210832568320
 
-## Available Scripts
 
-In the project directory, you can run:
-
-### `yarn start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
-
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `yarn test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `yarn build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+curl --request GET \
+     --url https://api.opensea.io/api/v1/asset/0xa342f5d851e866e18ff98f351f2c6637f4478db5/72177230440028127882110734035308912615268443000429468718652064783715499249664
--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
 import logo from './logo.svg';
 import './App.css';
+import { FreeCamera, Vector3, HemisphericLight, MeshBuilder, SceneLoader, Color3 } from "@babylonjs/core";
+import { useState} from 'react';
+import SceneComponent from "./SceneComponent";
+import { Engine, Scene } from 'react-babylonjs'
+// import 'babylonjs-loaders';
+import '@babylonjs/loaders';
+import ScaledModelWithProgress from './ScaledModelWithProgress';
+let box;
+const gltfPath = 'https://storage.opensea.io/files/e085da0987a623f329d9587723a12b8d.gltf';
+
+const onSceneReady = (scene) => {
+  // This creates and positions a free camera (non-mesh)
+  var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+
+  // This targets the camera to scene origin
+  camera.setTarget(Vector3.Zero());
+
+  const canvas = scene.getEngine().getRenderingCanvas();
+
+  // This attaches the camera to the canvas
+  camera.attachControl(canvas, true);
+
+  // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+  var light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+
+  // Default intensity is 1. Let's dim the light a small amount
+  light.intensity = 0.7;
+
+  // Our built-in 'box' shape.
+  box = MeshBuilder.CreateBox("box", { size: 2 }, scene);
+
+  // Move the box upward 1/2 its height
+  box.position.y = 1;
+
+  // Add gltf
+  // https://storage.opensea.io/files/e085da0987a623f329d9587723a12b8d.gltf
+
+  // BABYLON.SceneLoader.Append("./", "duck.gltf", scene, function (scene) {
+  //   // do something with the scene
+  // });
+  
+  SceneLoader.Append(gltfPath, '',  scene, (scene) => {
+    console.log('success');
+  });
+  // SceneLoader.Append('https://storage.opensea.io/files/e085da0987a623f329d9587723a12b8d.gltf', scene, (scene) => {
+  //   console.log('success');
+  // });
+  
+
+  // Our built-in 'ground' shape.
+  MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
+};
+
+const onRender = (scene) => {
+  if (box !== undefined) {
+    var deltaTimeInMillis = scene.getEngine().getDeltaTime();
+
+    const rpm = 10;
+    box.rotation.y += (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000);
+  }
+};
+
+
 
 function App() {
+  const [NFTUrls, setNFTUrls] = useState([gltfPath, 'https://storage.opensea.io/files/d3869b058e297fa014862a254fcccf02.gltf', 'https://storage.opensea.io/files/abb866f89bfb78f2cc7dd52406027e10.gltf']);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <script src="https://cdn.babylonjs.com/loaders/babylonjs.loaders.min.js"></script>
+      {/* <SceneComponent antialias onSceneReady={onSceneReady} onRender={onRender} id="my-canvas" /> */}
+      
+
+          <Engine antialias={true} adaptToDeviceRatio={true} canvasId="sample-canvas">
+            <Scene>
+              <arcRotateCamera name="camera1" alpha={Math.PI / 2} beta={Math.PI / 2} radius={9.0} target={Vector3.Zero()} minZ={0.001} />
+              <hemisphericLight name="light1" intensity={0.7} direction={Vector3.Up()} />
+              {/* <ScaledModelWithProgress rootUrl={`https://storage.opensea.io/files/`} sceneFilename="e085da0987a623f329d9587723a12b8d.gltf" scaleTo={1} 
+                progressBarColor={Color3.FromInts(255, 165, 0)} center={new Vector3(-2.5, 0, 0)}
+              /> */}
+              {
+                NFTUrls.map((url, index) => {
+                  const separatorIndex = url.lastIndexOf('/');
+                  const rootUrl = url.slice(0,separatorIndex + 1); // get until last '/'
+                  const filename = url.slice(separatorIndex + 1);
+                  return <ScaledModelWithProgress rootUrl={rootUrl} sceneFilename={filename} scaleTo={1} 
+progressBarColor={Color3.FromInts(255, 165, 0)} center={new Vector3(index, index , 0)}
+              />;
+                  
+                })
+              }
+              
+            </Scene>
+          </Engine>
+      {/* <input /> */}
+      {/* <button onClick={()=>{}}>SHOW</button> */}
     </div>
   );
 }
